aa815df8aa636feeff8ad5b44a840f9b54342569
skutner <sava.dumitru.daniel@gmail.com>
2019-07-17 14:53:35 +0300

    refactoring getBarMap

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 module.exports.Brick = require("./lib/Brick");
 module.exports.Archive = require("./lib/Archive");
 module.exports.ArchiveConfigurator = require("./lib/ArchiveConfigurator");
+module.exports.FolderBarMap = require("./lib/FolderBarMap");
+module.exports.createFsBarWorker = require("./lib/FsBarWorker").createFsBarWorker;
+
