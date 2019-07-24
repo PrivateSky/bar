21194cec8238f917b6f437eba414ed2b343da46b
skutner <sava.dumitru.daniel@gmail.com>
2019-07-24 16:16:20 +0300

    registered brick storage providers in ArchiveConfigurator

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 module.exports.Brick = require("./lib/Brick");
 module.exports.Archive = require("./lib/Archive");
-module.exports.ArchiveConfigurator = require("./lib/ArchiveConfigurator");
 module.exports.FolderBarMap = require("./lib/FolderBarMap");
 module.exports.createFsBarWorker = require("./lib/FsBarWorker").createFsBarWorker;
+const ArchiveConfigurator = require("./lib/ArchiveConfigurator");
+const createFolderBrickStorage = require("./lib/FolderBrickStorage").createFolderBrickStorage;
+const createFileBrickStorage = require("./lib/FileBrickStorage").createFileBrickStorage;
+ArchiveConfigurator.prototype.registerStorageProvider("FolderBrickStorage", createFolderBrickStorage);
+ArchiveConfigurator.prototype.registerStorageProvider("FileBrickStorage", createFileBrickStorage);
+module.exports.ArchiveConfigurator = ArchiveConfigurator;
 
