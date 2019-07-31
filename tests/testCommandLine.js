924e6e3e56e7fa03639416cd1d4eb607bf3bb9ef
BaditaGeorge <baditageorge@yahoo.com>
2019-07-31 18:20:00 +0300

    added another test case

--- tests/testCommandLine.js
+++ tests/testCommandLine.js
@@ -1,7 +1,7 @@
 const assert = require("double-check").assert;
 const CommandHandler = require('../lib/CommandHandler');
 
-let commandLine = [['bar','barx','-cf','name.bar','folderName'],['bar','barx','-x','-f','name.bar'],['bar','barx','-tf','name.bar'],['bar','barx','-czf','name.bar','folderName']];
+let commandLine = [['bar','barx','-cf','name.bar','folderName'],['bar','barx','-x','--file=name.bar'],['bar','barx','-tf','name.bar'],['bar','barx','-czf','name.bar','folderName'],['bar','barx','-cxf','a','b']];
 assert.callback("testCommandLine",(callback)=>{
     let commandHandler = new CommandHandler(commandLine[0]);
     let parameters = commandHandler.getParameters();
@@ -10,7 +10,7 @@ assert.callback("testCommandLine",(callback)=>{
     commandHandler = new CommandHandler(commandLine[1]);
     parameters = commandHandler.getParameters();
     flags = commandHandler.getFlags();
-    assert.true(parameters.length === 1 && flags.length === 2 && flags[0].toString() === '-x' && flags[1].toString() === '-f' && parameters[0].toString() === 'name.bar');
+    assert.true(parameters.length === 1 && flags.length === 2 && flags[0].toString() === '-x' && flags[1].toString() === '--file' && parameters[0].toString() === 'name.bar');
     commandHandler = new CommandHandler(commandLine[2]);
     parameters = commandHandler.getParameters();
     flags = commandHandler.getFlags();
@@ -19,5 +19,9 @@ assert.callback("testCommandLine",(callback)=>{
     parameters = commandHandler.getParameters();
     flags = commandHandler.getFlags();
     assert.true(parameters.length === 2 && flags.length === 1 && flags[0].toString() === '-czf' && parameters[0].toString() === 'name.bar' && parameters[1].toString() === 'folderName');
+    commandHandler = new CommandHandler(commandLine[4]);
+    parameters = commandHandler.getParameters();
+    flags = commandHandler.getFlags();
+    assert.true(typeof parameters === "undefined" && typeof flags === "undefined");
     callback();
 },1500);
