0e76b9f34d4e5e1c91aaa69f991c14d7fe0f7d16
BaditaGeorge <baditageorge@yahoo.com>
2019-08-01 17:27:25 +0300

    Refactoring archive Configurator

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 module.exports.Brick = require("./lib/Brick");
 module.exports.Archive = require("./lib/Archive");
 module.exports.FolderBarMap = require("./lib/FolderBarMap");
-module.exports.createFsBarWorker = require("./lib/FsBarWorker").createFsBarWorker;
+const createFsBarWorker = require("./lib/FsBarWorker").createFsBarWorker;
+module.exports.createFsBarWorker = createFsBarWorker;
 const ArchiveConfigurator = require("./lib/ArchiveConfigurator");
 const createFolderBrickStorage = require("./lib/FolderBrickStorage").createFolderBrickStorage;
 const createFileBrickStorage = require("./lib/FileBrickStorage").createFileBrickStorage;
 ArchiveConfigurator.prototype.registerStorageProvider("FolderBrickStorage", createFolderBrickStorage);
 ArchiveConfigurator.prototype.registerStorageProvider("FileBrickStorage", createFileBrickStorage);
+ArchiveConfigurator.prototype.registerDiskAdapter("FsAdapter", createFsBarWorker);
 module.exports.ArchiveConfigurator = ArchiveConfigurator;
 
