80bcfd1d69d7c142f52d24863ff271fcaa6a7818
skutner <sava.dumitru.daniel@gmail.com>
2019-08-23 14:15:08 +0300

    refactoring commandLineTest tests directory cleanup

--- tests/testCommandLine.js
+++ tests/testCommandLine.js
@@ -1,23 +1,52 @@
 const assert = require("double-check").assert;
 const CommandHandler = require('../lib/CommandHandler');
 
-let commandLine = [['bar','barx','-cf','name.bar','folderName'],['bar','barx','-x','--file=name.bar'],['bar','barx','-tf','name.bar'],['bar','barx','-czf','name.bar','folderName']];
+let commandLine = [['bar', '-cf', 'name.bar', 'folderName'], ['bar', '-c', '--file=name.bar', "folderName"], ['bar', '-tf', 'name.bar'], ['bar', '-t', '--file=name.bar'], ['bar', '-czf', 'name.bar', 'folderName']];
 assert.callback("testCommandLine",(callback)=>{
     let commandHandler = new CommandHandler(commandLine[0]);
     let parameters = commandHandler.getParameters();
     let flags = commandHandler.getFlags();
-    assert.true(parameters.length === 2 && flags[0].toString() === '-cf' && parameters[0].toString() === 'name.bar' && parameters[1].toString() === 'folderName' && flags.length === 1 && commandHandler.getCommand() === 'cf');
+    let command = commandHandler.getCommand();
+
+    assert.true(parameters[0] === 'name.bar' && parameters[1] === 'folderName', "Wrong parameters");
+    assert.true(flags[0].toString() === '-cf', "Wrong flag");
+    assert.true(command === 'cf', "Wrong command");
+
     commandHandler = new CommandHandler(commandLine[1]);
     parameters = commandHandler.getParameters();
     flags = commandHandler.getFlags();
-    assert.true(parameters.length === 1 && flags.length === 2 && flags[0].toString() === '-x' && flags[1].toString() === '--file' && parameters[0].toString() === 'name.bar' && commandHandler.getCommand() === 'xf');
+    command = commandHandler.getCommand();
+
+    assert.true(parameters[0] === 'name.bar', "Wrong parameters");
+    assert.true(flags[0]=== '-c', "Wrong flags");
+    assert.true(command === 'cf', "Wrong command");
+
     commandHandler = new CommandHandler(commandLine[2]);
     parameters = commandHandler.getParameters();
     flags = commandHandler.getFlags();
-    assert.true(parameters.length === 1 && flags.length === 1 && flags[0].toString() === '-tf' && parameters[0].toString() === 'name.bar' && commandHandler.getCommand() === 'tf');
+    command = commandHandler.getCommand();
+
+    assert.true(parameters[0] === 'name.bar');
+    assert.true(flags[0] === '-tf', "Wrong flags");
+    assert.true(command === 'tf', "Wrong command");
+
     commandHandler = new CommandHandler(commandLine[3]);
     parameters = commandHandler.getParameters();
     flags = commandHandler.getFlags();
-     assert.true(parameters.length === 2 && flags.length === 1 && flags[0].toString() === '-czf' && parameters[0].toString() === 'name.bar' && parameters[1].toString() === 'folderName' && commandHandler.getCommand() ==='czf');
+    command = commandHandler.getCommand();
+
+    assert.true(parameters[0] === 'name.bar');
+    assert.true(flags[0] === '-t' && flags[1] === '--file', "Wrong flags");
+    assert.true(command === 'tf', "Wrong command");
+
+    commandHandler = new CommandHandler(commandLine[4]);
+    parameters = commandHandler.getParameters();
+    flags = commandHandler.getFlags();
+    command = commandHandler.getCommand();
+
+    assert.true(parameters[0] === 'name.bar' && parameters[1] === 'folderName', "Wrong parameters");
+    assert.true(flags[0] === '-czf', "Wrong flags");
+    assert.true(command === 'czf', "Wrong command");
+
     callback();
 },1500);
