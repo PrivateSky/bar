008044a809ea90b6e1414eef3844b3693c7b4da4
skutner <sava.dumitru.daniel@gmail.com>
2019-09-03 16:22:26 +0300

    using bigint for bar map offset

--- tests/addFolderExtractFileTest.js
+++ tests/addFolderExtractFileTest.js
@@ -4,6 +4,7 @@ const utils = require("./utils/utils");
 const Archive = require("../lib/Archive");
 const fs = require("fs");
 
+const folderPath = path.resolve("fld");
 const filePath = path.resolve("fld/a.txt");
 let savePath = "dot";
 
@@ -13,7 +14,7 @@ const files = [
     "fld/a.txt", "fld/b.txt", "fld/c.txt"
 ];
 
-const text = ["asta e un text", "ana are mere", "hahahaha"];
+const text = ["asta e un text?", "ana are mere", "hahahaha"];
 
 const createFileBrickStorage = require("../lib/FileBrickStorage").createFileBrickStorage;
 const createFsAdapter = require("../lib/FsAdapter").createFsAdapter;
@@ -26,30 +27,25 @@ ArchiveConfigurator.prototype.registerFsAdapter("fsAdapter", createFsAdapter);
 const archiveConfigurator = new ArchiveConfigurator();
 archiveConfigurator.setStorageProvider("FileBrickStorage", savePath);
 archiveConfigurator.setFsAdapter("fsAdapter");
-archiveConfigurator.setBufferSize(65535);
+archiveConfigurator.setBufferSize(2);
 
 const archive = new Archive(archiveConfigurator);
 
-assert.callback("AddExtractFileTest", (callback) => {
+assert.callback("AddFolderExtractFile", (callback) => {
     utils.ensureFilesExist(folders, files, text, (err) => {
         assert.true(err === null || typeof err === "undefined", "Failed to create folder hierarchy.");
 
-        utils.computeFileHash(filePath, (err, initialHashes) => {
-            assert.true(err === null || typeof err === "undefined", "Received error");
-
-            archive.addFile(filePath, (err) => {
+            archive.addFolder(folderPath, (err) => {
                 assert.true(err === null || typeof err === "undefined", "Failed to archive file.");
 
-                fs.unlink(filePath, (err) => {
+                utils.deleteFolders(folders, (err) => {
                     assert.true(err === null || typeof err === "undefined", "Failed to delete file");
 
                     archive.extractFile(filePath, (err) => {
+                        if (err) {
+                            throw err;
+                        }
                         assert.true(err === null || typeof err === "undefined", "Failed to extract file.");
-
-                        utils.computeFileHash(filePath, (err, decompressedHashes) => {
-                            assert.true(err === null || typeof err === "undefined", "Failed to compute folders hashes");
-                            assert.true(initialHashes === decompressedHashes, "Files are not identical");
-
                             utils.deleteFolders(folders, (err) => {
                                 assert.true(err === null || typeof err === "undefined", "Failed to delete test folders");
 
@@ -62,8 +58,6 @@ assert.callback("AddExtractFileTest", (callback) => {
                         });
                     });
                 });
-            });
         });
-    });
 }, 1500);
 
