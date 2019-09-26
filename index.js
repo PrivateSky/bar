1f37221656231e507eb978c2e2b0e66c51eb7c40
skutner <sava.dumitru.daniel@gmail.com>
2019-09-26 15:49:13 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 module.exports.Brick = require("./lib/Brick");
 module.exports.Archive = require("./lib/Archive");
 module.exports.FolderBarMap = require("./lib/FolderBarMap");
-const createFsBarWorker = require("./lib/FsAdapter").createFsAdapter;
-module.exports.createFsBarWorker = createFsBarWorker;
+const createFsAdapter = require("./lib/FsAdapter").createFsAdapter;
+module.exports.createFsAdapter = createFsAdapter;
 const ArchiveConfigurator = require("./lib/ArchiveConfigurator");
 const createFolderBrickStorage = require("./lib/FolderBrickStorage").createFolderBrickStorage;
 const createFileBrickStorage = require("./lib/FileBrickStorage").createFileBrickStorage;
 ArchiveConfigurator.prototype.registerStorageProvider("FolderBrickStorage", createFolderBrickStorage);
 ArchiveConfigurator.prototype.registerStorageProvider("FileBrickStorage", createFileBrickStorage);
-ArchiveConfigurator.prototype.registerFsAdapter("FsAdapter", createFsBarWorker);
+ArchiveConfigurator.prototype.registerFsAdapter("FsAdapter", createFsAdapter);
 module.exports.ArchiveConfigurator = ArchiveConfigurator;
 
