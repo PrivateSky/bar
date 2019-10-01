4a2ef6a90d0e23d7d0cad0911498f4441d3aa70e
skutner <sava.dumitru.daniel@gmail.com>
2019-10-01 17:33:43 +0300

    added function clone in Archive

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-module.exports.Brick = require("./lib/Brick");
-module.exports.Archive = require("./lib/Archive");
-module.exports.FolderBarMap = require("./lib/FolderBarMap");
 const createFsAdapter = require("./lib/FsAdapter").createFsAdapter;
-module.exports.createFsAdapter = createFsAdapter;
+
 const ArchiveConfigurator = require("./lib/ArchiveConfigurator");
 const createFolderBrickStorage = require("./lib/FolderBrickStorage").createFolderBrickStorage;
 const createFileBrickStorage = require("./lib/FileBrickStorage").createFileBrickStorage;
+
 ArchiveConfigurator.prototype.registerStorageProvider("FolderBrickStorage", createFolderBrickStorage);
 ArchiveConfigurator.prototype.registerStorageProvider("FileBrickStorage", createFileBrickStorage);
 ArchiveConfigurator.prototype.registerFsAdapter("FsAdapter", createFsAdapter);
-module.exports.ArchiveConfigurator = ArchiveConfigurator;
 
+module.exports.ArchiveConfigurator = ArchiveConfigurator;
+module.exports.Brick = require("./lib/Brick");
+module.exports.Archive = require("./lib/Archive");
+module.exports.FolderBarMap = require("./lib/FolderBarMap");
+module.exports.createFolderBrickStorage = createFolderBrickStorage;
+module.exports.createFileBrickStorage = createFileBrickStorage;
+module.exports.createFsAdapter = createFsAdapter;
