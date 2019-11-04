d98e3ed86f28670f4d1137a4764f3836d85711ad
BaditaGeorge <baditageorge@yahoo.com>
2019-11-04 11:52:12 +0200

    Working on update function.

--- tests/updateFileTest.js
+++ tests/updateFileTest.js
@@ -12,45 +12,38 @@ const crypto = require("crypto");
 const path = require("path");
 
 double_check.createTestFolder("bar_test_folder", (err, testFolder) => {
-
     const folderPath = path.join(testFolder, "fld");
     const filePath = path.join(testFolder, "fld/a.txt");
     let savePath = path.join(testFolder, "dot");
 
 
     const folders = ["fld"].map(folder => path.join(testFolder, folder));
-    const files = ["fld/a.txt", "fld/b.txt", "fld/c.txt"].map(file => path.join(testFolder, file));
+    const files = [path.join('fld','a.txt'), path.join('fld','b.txt'), path.join('fld','c.txt')].map(file => path.join(testFolder, file));
 
     const text = ["asta e un text?", "ana are mere", "hahahaha"];
 
     const archiveConfigurator = new ArchiveConfigurator();
     archiveConfigurator.setStorageProvider("FileBrickStorage", savePath);
     archiveConfigurator.setFsAdapter("FsAdapter");
-    archiveConfigurator.setBufferSize(2);
-
+    archiveConfigurator.setBufferSize(3);
     const archive = new Archive(archiveConfigurator);
 
 
     assert.callback("AddFolderExtractFile", (callback) => {
         double_check.ensureFilesExist(folders, files, text, (err) => {
             assert.true(err === null || typeof err === "undefined", "Failed to create folder hierarchy.");
-
             archive.addFolder(folderPath, (err) => {
                 assert.true(err === null || typeof err === "undefined", "Failed to archive file.");
 
-                double_check.deleteFoldersSync(folders);
-                assert.true(err === null || typeof err === "undefined", "Failed to delete file");
-
-                archive.extractFile(filePath, (err) => {
-                    assert.true(err === null || typeof err === "undefined", "Failed to extract file.");
-
-                    double_check.deleteFoldersSync(folders);
-
-                    fs.unlink(savePath, (err) => {
-                        assert.true(err === null || typeof err === "undefined", "Failed to delete file " + savePath);
-
-                        callback();
-                    });
+                fs.writeFileSync(path.join(testFolder,path.join('fld','d.txt')),'asta este un test de UPDATE!xoxox');
+                console.log(testFolder);
+                archive.update(folders[0],(err,msg)=>{
+                    if(err){
+                        throw err;
+                    }
+                    console.log(msg);
+                    console.log('NothingTOBidon');
+                    callback();
                 });
             });
         });
