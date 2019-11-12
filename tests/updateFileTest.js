e1293aa7e57ef8302cba31546ae09f9b3ccb23d7
BaditaGeorge <baditageorge@yahoo.com>
2019-11-12 16:18:17 +0200

    Test for Update Function.

--- tests/updateFileTest.js
+++ tests/updateFileTest.js
@@ -1,51 +1,67 @@
+const path = require("path");
+
 const double_check = require("../../../modules/double-check");
 const assert = double_check.assert;
 const Archive = require("../lib/Archive");
 const ArchiveConfigurator = require("../lib/ArchiveConfigurator");
-const createFileBrickStorage = require("../lib/FileBrickStorage").createFileBrickStorage;
 const createFolderBrickStorage = require("../lib/FolderBrickStorage").createFolderBrickStorage;
 const createFsAdapter = require("../lib/FsAdapter").createFsAdapter;
 ArchiveConfigurator.prototype.registerStorageProvider("FolderBrickStorage", createFolderBrickStorage);
 ArchiveConfigurator.prototype.registerFsAdapter("FsAdapter", createFsAdapter);
 
 const fs = require("fs");
-const crypto = require("crypto");
-const path = require("path");
 
-//double_check.createTestFolder("bar_test_folder", (err, testFolder) => {
+double_check.createTestFolder("bar_test_folder", (err, testFolder) => {
 
-    const folderPath =  "fld";
-    let savePath =  "dot";
+    let savePath = path.join(testFolder, "dot");
 
 
-    const folders = ["fld","dot"];
-    const files = [path.join('fld','a.txt'), path.join('fld','b.txt'), path.join('fld','c.txt')];
+    const folders = ["fld", "dot"].map(folder => path.join(testFolder, folder));
+    const files = ["a.txt", "b.txt", "c.txt"].map(file => path.join(testFolder, 'fld', file));
 
     const text = ["asta e un text?", "ana are mere", "hahahaha"];
 
     const archiveConfigurator = new ArchiveConfigurator();
     archiveConfigurator.setStorageProvider("FolderBrickStorage", savePath);
     archiveConfigurator.setFsAdapter("FsAdapter");
-    archiveConfigurator.setBufferSize(3);
+    archiveConfigurator.setEncryptionAlgorithm("aes-256-gcm");
+    archiveConfigurator.setBufferSize(2);
+
     const archive = new Archive(archiveConfigurator);
 
 
-    assert.callback("AddFolderExtractFile", (callback) => {
+    assert.callback("updateFileTest", (callback) => {
         double_check.ensureFilesExist(folders, files, text, (err) => {
             assert.true(err === null || typeof err === "undefined", "Failed to create folder hierarchy.");
-            archive.addFolder(folderPath, (err) => {
+
+            archive.addFolder(folders[0], (err) => {
                 assert.true(err === null || typeof err === "undefined", "Failed to archive file.");
 
-                fs.writeFileSync(path.join('fld','d.txt'),'asta este un test de UPDATE!xoxox');
-                archive.update(folderPath,(err)=>{
-                    if(err){
-                        throw err;
-                    }
-                    console.log('DAN');
-                    callback();
+
+                fs.writeFileSync(path.join(testFolder, 'fld','d.txt'), 'Acesta este un test de UPDATE!');
+
+                archive.update(folders[0], (err, digest) => {
+                    double_check.computeFoldersHashes(folders[0], (err, initialHashes) => {
+                        assert.true(err === null || typeof err === "undefined", "Received error");
+                        double_check.deleteFoldersSync(folders[0]);
+                        archive.extractFolder(folders[1], (err) => {
+                            assert.true(err === null || typeof err === "undefined", "Failed to extract file.");
+
+                            double_check.computeFoldersHashes(folders[0], (err, decompressedHashes) => {
+                                assert.true(err === null || typeof err === "undefined", "Failed to compute folders hashes");
+                                assert.true(initialHashes === decompressedHashes, "Files are not identical");
+
+                                double_check.deleteFoldersSync(folders);
+
+                                callback();
+                            });
+                        });
+                    });
                 });
             });
         });
-    }, 1500);
+    }, 2000);
+});
+
 
 
