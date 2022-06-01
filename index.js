63ef767b3eafc8571430fca1d4650c0976057806
Daniel Visoiu <9691701+daniel-visoiu@users.noreply.github.com>
2022-06-01 22:16:29 +0300

    Exposed method for creating brickStorageService. Added Archive methods for writing and reading (as stream) big files.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const ArchiveConfigurator = require("./lib/ArchiveConfigurator");
 const createFolderBrickStorage = require("./lib/obsolete/FolderBrickStorage").createFolderBrickStorage;
 const createFileBrickStorage = require("./lib/obsolete/FileBrickStorage").createFileBrickStorage;
+const BrickStorageService = require('./lib/BrickStorageService').Service;
+const BrickMapController = require('./lib/BrickMapController');
 
 ArchiveConfigurator.prototype.registerStorageProvider("FolderBrickStorage", createFolderBrickStorage);
 ArchiveConfigurator.prototype.registerStorageProvider("FileBrickStorage", createFileBrickStorage);
@@ -35,3 +37,44 @@ module.exports.BrickMapStrategyFactory = require('./lib/BrickMapStrategy');
 module.exports.BrickMapStrategyMixin = require('./lib/BrickMapStrategy/BrickMapStrategyMixin');
 module.exports.createFolderBrickStorage = createFolderBrickStorage;
 module.exports.createFileBrickStorage = createFileBrickStorage;
+
+module.exports.createBrickStorageService = (archiveConfigurator, keySSI) => {
+    const brickStorageService = new BrickStorageService({
+        cache: archiveConfigurator.getCache(),
+        bufferSize: archiveConfigurator.getBufferSize(),
+        keySSI,
+
+        brickFactoryFunction: (encrypt) => {
+            const Brick = require("./lib/Brick");
+            encrypt = (typeof encrypt === 'undefined') ? true : !!encrypt;
+            // Strip the encryption key from the SeedSSI
+            return new Brick({templateKeySSI: keySSI, encrypt});
+        },
+
+        brickDataExtractorCallback: (brickMeta, brick, callback) => {
+            brick.setTemplateKeySSI(keySSI);
+
+            function extractData() {
+                const brickEncryptionKeySSI = brickMapController.getBrickEncryptionKeySSI(brickMeta);
+                brick.setKeySSI(brickEncryptionKeySSI);
+                brick.getRawData(callback);
+            }
+
+            if (refreshInProgress) {
+                return waitIfDSUIsRefreshing(() => {
+                    extractData();
+                })
+            }
+            extractData();
+        },
+
+        fsAdapter: archiveConfigurator.getFsAdapter()
+    });
+    const brickMapController = new BrickMapController({
+        config: archiveConfigurator,
+        brickStorageService,
+        keySSI
+    });
+
+    return brickStorageService;
+};
